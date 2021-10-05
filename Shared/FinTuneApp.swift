0b51f9245945ccefc0a4a6a999b7455a89d609b9
Jack Caulfield <jackcauliflower@gmail.com>
2021-10-05 13:38:05 -0500

    Initial Commit

---
+++ Shared/FinTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinTuneApp.swift
+//  Shared
+//
+//  Created by Jack Caulfield on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct FinTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
